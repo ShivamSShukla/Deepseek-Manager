7dfa7711ee48d0093643d000ecad72b6c1cab183
Shivam S <info.sshivam9819@gmail.com>
2026-02-06 10:35:26 +0530

    Add modules/index.js

---
+++ modules/index.js
@@ -0,0 +1,15 @@
+import ChatStorageManager from './ChatStorageManager.js';
+import ChatParser from './ChatParser.js';
+import ProjectBuilder from './ProjectBuilder.js';
+import ZIPBuilder from './ZIPBuilder.js';
+import ExportManager from './ExportManager.js';
+import UIManager from './UIManager.js';
+
+export {
+  ChatStorageManager,
+  ChatParser,
+  ProjectBuilder,
+  ZIPBuilder,
+  ExportManager,
+  UIManager
+};
